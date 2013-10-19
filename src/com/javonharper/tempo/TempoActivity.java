300ac135ab3697329092d18bf58db79238dabc11
Javon Harper <javon.d.harper@gmail.com>
2013-10-18 21:29:26 -0400

    Saturate button as well

--- src/com/javonharper/tempo/TempoActivity.java
+++ src/com/javonharper/tempo/TempoActivity.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
